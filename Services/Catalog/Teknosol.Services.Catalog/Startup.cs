f7b958e178aebac237c5bbb2de23c0860118718a
guven.uysal <guven.uysal@teknosol.com.tr>
2022-04-14 22:22:25 +0300

    Added home controller for swagger

--- Services/Catalog/Teknosol.Services.Catalog/Startup.cs
+++ Services/Catalog/Teknosol.Services.Catalog/Startup.cs
@@ -31,6 +31,7 @@ namespace Teknosol.Services.Catalog
             services.AddTransient<ICourseService, CourseService>();
 
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Teknosol.Services.Catalog", Version = "v1" }); });
+            services.AddRouting(options => options.LowercaseUrls = true);//lowercase routing
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +50,7 @@ namespace Teknosol.Services.Catalog
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints => { endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}"); });
         }
     }
 }
